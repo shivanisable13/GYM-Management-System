31cedb78ae855fb6848e28802011ea009f60ae2c
shivanisable13 <shivanisable075@gmail.com>
2024-05-29 19:13:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+gsap.from("#nav",{
+    y:-100,
+    duration:1,
+    delay:1,
+    opacity:0
+})
+
+
+gsap.from("#text-section h1",{
+    x:-60,
+    duration:1,
+    delay:1,
+    opacity:0
+})
+
+gsap.from(".heading2",{
+    x:-60,
+    duration:1,
+    delay:2,
+    opacity:0
+})
+
